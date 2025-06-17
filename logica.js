87e9710b7104e7e6c43f57005cfd8156351117fd
rayssamatos13 <rayssamatosmariarayssa@gmail.com>
2025-06-17 20:10:33 -0300

    paulin

--- logica.js
+++ logica.js
@@ -31,11 +31,13 @@ listaPerguntas = [
     ]
 
 objPergunta = null;
+perguntaAtual = 0
 
 telaInicial = document.querySelector(".tela-inicial")
 telaPergunta = document.querySelector(".tela-pergunta")
 telaAcertou = document.querySelector(".tela-acertou")
 telaErrou = document.querySelector(".tela-errou")
+telaFinal= document.querySelector(".tela-final")
 
 botaoIniciar = document.querySelector(".iniciar")
 botaoIniciar.addEventListener("click", iniciarJogo)
@@ -49,7 +51,17 @@ botao3 = document.querySelector(".opcao3");
 
 telaAcertou.addEventListener("click", () => {
     telaAcertou.style.display = "none"
-    iniciarJogo()
+    if (perguntaAtual == 3) {
+        mostrarFinal ()
+    }
+    else {
+        iniciarJogo()
+    }
+})
+telaFinal.addEventListener ("click", () => {
+  telaFinal.style.display = "none"
+  telaInicial.style.display = "block"
+  perguntaAtual = 0
 })
 
 botao0.addEventListener("click", () => {
@@ -92,35 +104,42 @@ function iniciarJogo() {
 }
 
 function mostrarPergunta() {
-    objPergunta = listaPerguntas[0]
-    
+    objPergunta = listaPerguntas[perguntaAtual]
     textoPergunta = document.createTextNode(objPergunta.pergunta);
-    
+    divPergunta.innerHTML = ""
     divPergunta.appendChild(textoPergunta);
     
     txtOpcao1 = document.createTextNode(objPergunta.opcoes[0]);
     botao0 = document.querySelector(".opcao0")
+    botao0.innerHTML = ""
     botao0.appendChild(txtOpcao1);
     
     txtOpcao2 = document.createTextNode(objPergunta.opcoes[1]);
     botao1 = document.querySelector(".opcao1")
+    botao1.innerHTML = ""
     botao1.appendChild(txtOpcao2);
     
     txtOpcao3 = document.createTextNode(objPergunta.opcoes[2]);
     botao2 = document.querySelector(".opcao2")
+    botao2.innerHTML = ""
     botao2.appendChild(txtOpcao3);
     
     txtOpcao4 = document.createTextNode(objPergunta.opcoes[3]);
     botao3 = document.querySelector(".opcao3")
+    botao3.innerHTML = ""
     botao3.appendChild(txtOpcao4);
     }
     
 function mostrarAcertou() {
     telaPergunta.style.display = "none"
     telaAcertou.style.display = "block"
+    perguntaAtual++
 }
-
 function mostrarErrou() {
     telaPergunta.style.display = "none"
     telaErrou.style.display = "block"
+}
+function mostrarFinal() {
+    telaPergunta.style.display = "none"
+    telaFinal.style.display = "block"
 }
